5b9a81e782dceb39441779676efc5eba31be1199
Michael Girgis <michael.girgis@live.ca>
2023-11-16 23:05:13 -0500

    completed script and readme files

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# Module-3-Challenge
+Module-3-Challenge
 JavaScript Challenge: Password Generator
+
+
+Description:
+
+For this Challenge we were asked to create an applicatin that an employee can use to generate a random password based on criteria they've selected. I've included the provided Acceptance Criteria below.
+
+GIVEN I need a new, secure password
+WHEN I click the button to generate a password
+THEN I am presented with a series of prompts for password criteria
+WHEN prompted for password criteria
+THEN I select which criteria to include in the password
+WHEN prompted for the length of the password
+THEN I choose a length of at least 8 characters and no more than 128 characters
+WHEN asked for character types to include in the password
+THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
+WHEN I answer each prompt
+THEN my input should be validated and at least one character type should be selected
+WHEN all prompts are answered
+THEN a password is generated that matches the selected criteria
+WHEN the password is generated
+THEN the password is either displayed in an alert or written to the page
+
+
+Credits:
+
+The below resources were referenced to learn how to accomplish the associated tasks.
+
+Generate initial window prompt after clicking "generate password" on screen.
+
+https://www.shecodes.io/athena/11102-how-to-trigger-a-prompt-on-button-click-in-javascript#:~:text=First%2C%20you'll%20need%20to,you%20can%20display%20the%20prompt.&text=%2F%2F%20Get%20the%20button%20element,for%20the%20click%20event%20button.
+
+
+Declare empty array and add values to it dynamicaly using ".push".
+
+https://www.altcademy.com/blog/how-to-create-an-empty-array-in-javascript/#why-use-empty-arrays
+
+
+Display contents of array as a string (without commas between values). This was used to display the final generated password.
+
+https://www.w3schools.com/js/js_array_methods.asp
+
+
+License:
+
+MIT License
+
+
+Screen shot of initial prompt:
+
+![initial prompt](<Screenshot (7).png>)
--- script.js
+++ script.js
@@ -1,14 +1,87 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//Declaration of variables and arrays
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g','h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                  'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 
-  passwordText.value = password;
+var uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                  'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+                  
+var numeric = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-}
+var special = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=']; 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+let character;
+
+let index;
+
+let password = [];
+
+//Initial prompt requesting user enter the number of desired characters in the password.
+
+const button = document.querySelector("button");
+
+button.addEventListener("click", function() {
+  const userLength = prompt("Enter character length. (Must be at least 8 and not more than 128.): ");
+  console.log(userLength);
+  
+  //Prompt asking if user wants to include lowercase characters.
+
+  var userLowercase = window.prompt("Would you like to include lowercase characters? (Y/N):");
+
+  userLowercase = userLowercase.toUpperCase();
+
+ //Prompt asking if user wants to include uppercase characters.
+
+  var userUppercase = window.prompt("Would you like to include uppercase characters? (Y/N):");
+
+  userUppercase = userUppercase.toUpperCase();
+  
+//Prompt asking if user wants to include numeric characters.
+
+  var userNumeric = window.prompt("Would you like to include numeric characters? (Y/N):");
+
+  userNumeric = userNumeric.toUpperCase();
+  
+//Prompt asking if user wants to include special characters.
+
+  var userSpecial = window.prompt("Would you like to include special characters? (Y/N):");
+
+  userSpecial = userSpecial.toUpperCase();
+
+//Code to generate unique password based on user's selections.
+
+  for (var i=1; i <= userLength; i++) {
+    if (userLowercase === "Y") {
+      index = Math.floor(Math.random() * lowercase.length);
+      character = lowercase[index];
+      password.push(character);
+    } else if (userUppercase === "Y"){
+      index = Math.floor(Math.random() * uppercase.length);
+      character = uppercase[index];
+      password.push(character);
+    } else if (userNumeric === "Y"){
+      index = Math.floor(Math.random() * numeric.length);
+      character = numeric[index];
+      password.push(character);
+    } else if (userSpecial === "Y"){
+      index = Math.floor(Math.random() * special.length);
+      character = special[index];
+      password.push(character);
+    } else {
+      console.log(password);
+      }
+    
+  };
+
+//Password displayed as alert.
+
+console.log(password.join(''));
+window.alert("Your password is: " + password.join(''));
+  
+  
+});
+
+ 
+
+
+ 
